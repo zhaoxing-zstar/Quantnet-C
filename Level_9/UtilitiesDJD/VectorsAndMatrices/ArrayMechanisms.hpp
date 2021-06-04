20d035e60832eb697f614b742476b1ca12fd1f60
Zstar <60384317+zhaoxing-zstar@users.noreply.github.com>
2021-06-04 10:41:40 +0800

    Add files via upload

---
+++ Level_9/UtilitiesDJD/VectorsAndMatrices/ArrayMechanisms.hpp
@@ -0,0 +1,21 @@
+/*
+ * @Descripttion: 
+ * @version: 
+ * @Author: Zstar
+ * @LastEditors: Zstar
+ */
+// ArrayMechanisms.hpp
+//
+
+// (C) Datasim Education BV 2003-2012
+
+#ifndef ArrayMechanisms_hpp
+#define ArrayMechanisms_hpp
+
+#include "Vector.cpp"
+
+////////////// Useful and Basic Print Functions ////////////////////////////////////////////////////
+template <typename V, typename I> void print(const Array<V,I>& v);
+template <typename V, typename I> void print(const Vector<V,I>& v);
+
+#endif
